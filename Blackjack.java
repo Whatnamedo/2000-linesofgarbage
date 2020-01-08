85fd709ab9ffa186951d348ce9a740701f52e73d
Nick3411 <59579161+Nick3411@users.noreply.github.com>
2020-01-08 15:30:35 -0500

    Update Blackjack.java
    Fixed double down

--- Blackjack.java
+++ Blackjack.java
@@ -99,6 +99,24 @@ public class Blackjack {
 					System.out.println("\n" + playerHand); // display the player's hand
 					TimeUnit.SECONDS.sleep(2); // pause for 2 seconds
 				}
+				if (turn == 1 && (str.equals("double down") || str.equals("dd")))
+				{
+					while (playerHand.getTotalValue() > 21 && playerHand.getNumAces() > 0) // if the player's hand is above 21 and he has an ace
+					{
+						System.out.println("\n" + playerHand); // display the player's hand
+						TimeUnit.SECONDS.sleep(2); // pause for 2 seconds
+						playerHand.subtractTotalValue(); // remove 10 from the value of the hand
+						break;
+					}
+					if (playerHand.getTotalValue() > 21) // if the player's hand is above 21 with no aces
+					{
+						TimeUnit.SECONDS.sleep(1); // pause for 1 second
+						System.out.println("\nYour hand is busted!\nThe dealer has won this round!"); 
+						TimeUnit.SECONDS.sleep(1); // pause for 1 second
+						System.out.println("--------------------------");
+						return (-1 * (bet/2)); // bet doesn't increase since it is being added by zero
+					}
+				}
 				if (str.equals("stand") || turn == 1 && (str.equals("double down") || str.equals("dd"))) // if the player decides to stand
 				{
 					System.out.println("\nDealers turn:");
