cb89f66b713703f927ea2be75f1b74d6aad3a9d4
Nick3411 <59579161+Nick3411@users.noreply.github.com>
2020-01-16 14:16:56 -0500

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
-public class Hand {
-	ArrayList<Card> cards = new ArrayList<Card>();  // stores all the cards of the hand
-	private int totalValue = 0;  // total value of the hand
+public class Hand
+{
+	ArrayList<Card> cards = new ArrayList<Card>(); // stores all the cards of the hand
+	private int totalValue = 0; // total value of the hand
 	boolean isDealer; // if the hand belongs to a dealer or a player
 	private String handValue = ""; // string to store the cards, used in the toString method
 	private String dealValue = "";
@@ -17,27 +18,38 @@ public class Hand {
 			handValue += "Card: ???\n\t";
 			dealValue += in + "\n\t";
 		}
-		else // if the hand belongs to the player or the dealer already has a card, the card will be added to a string variable
+		else // if the hand belongs to the player or the dealer already has a card, the card
+				// will be added to a string variable
 		{
 			handValue += in + "\n\t";
 			dealValue += in + "\n\t";
 		}
 		cards.add(in); // adds the card to the array
-		if (in.getRank() == 11 || in.getRank() == 12 || in.getRank() ==13)  // if the card is a jack, queen, king then the value of the card will be 10
+		if (in.getRank() == 11 || in.getRank() == 12 || in.getRank() == 13) // if the card is a jack, queen, king then
+																			// the value of the card will be 10
 		{
 			totalValue += 10;
-			temp += 10;
+			if (cards.size() == 1)
+			{
+				temp += 10;
+			}
 		}
 		else
 		{
 			totalValue += in.getRank();
-			temp += in.getRank(); // finds the numerical value of the card and adds it to the total value of the hand
-		}		
-		if (in.getRank() == 1) 
+			if (cards.size() == 1)
+			{
+				temp += in.getRank();
+			}
+		}
+		if (in.getRank() == 1)
 		{
 			totalValue += 10;
-			temp += 10;
-			numAces ++;
+			if (cards.size() == 1)
+			{
+				temp += 10;
+			}
+			numAces++;
 		}
 	}
 	
@@ -53,7 +65,7 @@ public class Hand {
 	
 	public void subtractAce()
 	{
-		numAces --;
+		numAces--;
 	}
 	
 	public int getSize()
@@ -78,12 +90,12 @@ public class Hand {
 	
 	public void subtractTotalValue()
 	{
-	     totalValue -= 10;
-	     if (cards.size() == 1)
-	     {
-		temp -= 10;
-	     }
-	     numAces --;
+		totalValue -= 10;
+		if (cards.size() == 1)
+		{
+			temp -= 10;
+		}
+		numAces--;
 	}
 	
 	public void show()
@@ -96,7 +108,7 @@ public class Hand {
 		return show;
 	}
 	
-	public Hand (boolean d) // constructor
+	public Hand(boolean d) // constructor
 	{
 		isDealer = d;
 	}
@@ -111,6 +123,6 @@ public class Hand {
 		{
 			return ("Current Total: " + totalValue);
 		}
-		return ("Current Total: " + (totalValue-temp));
-	}    
+		return ("Current Total: " + (totalValue - temp));
+	}
 }
